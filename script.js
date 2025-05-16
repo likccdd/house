a9f672012333a99a6b186468622ff9f59905290f
Ivan Filippenko <sevcenkoivan395@gmail.com>
2025-05-16 22:22:07 +0300

    made a script to switch countries

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 const mainCard = document.querySelectorAll('.main-card')
+const buttonCountries = document.querySelectorAll('.card-button-item')
+let currentIndex = 0
+
+const show = (index) => {
+    mainCard[currentIndex].classList.remove('is-active')
+    mainCard[index].classList.add('is-active')
+    currentIndex = index
+}
+
+buttonCountries.forEach(e => {
+    e.addEventListener('click', () => {
+        if (e.classList.contains('france')) {
+            show(0)
+        } else if (e.classList.contains('germany')) {
+            show(1)
+        } else if (e.classList.contains('england')) {
+            show(2)
+        }
+    })
+})
+
+show(currentIndex)
